134eedb675b2e78ede0da246daac53e72158e09c
songzhiyuan <www.1319324030@qq.com>
2016-06-20 11:46:40 +0800

    char
    字符串的几种基本操作

---
+++ char/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  char
+//
+//  Created by 20141105054 on 16/6/20.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+import Foundation
+
+var a:String="s"
+var b:String="zy"
+var c:String=a+b
+print(c)
+var d:String="Welcone\(c)!"
+print(d)
+var str:String="my name is "
+str += "szy"
+print(str)
+
